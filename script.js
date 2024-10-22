47ba0103547793e1e67723c4193d00d5c027570a
Umesh Deogharia <143274287+Umesh-Deogharia@users.noreply.github.com>
2024-10-22 21:19:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
 //your code here
+let images = document.querySelectorAll('.image');
+let dragElem = null;
+
+let image1 = document.querySelector('#drag1');
+let parent = document.querySelector('#parent');
+
+images.forEach((elem)=>{
+    elem.addEventListener("dragstart",(e)=>{
+    dragElem = e.target;
+    console.log(e.target);
+    console.log('drag Started');
+});
+})
+
+
+parent.addEventListener("dragover", (e) => {
+  e.preventDefault();
+  console.log("dragover", e.target);
+});
+
+
+
+ parent.addEventListener('drop',(e)=>{
+    e.preventDefault();
+
+    // let dragTarget = e.target;
+    // let dragOver = dragElem;
+
+    // console.log('DropElem',dragOver)
+
+
+     if (dragElem) {
+        parent.appendChild(dragElem);
+        dragElem = null;
+     }
+ })
